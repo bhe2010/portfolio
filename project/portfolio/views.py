f0c6df61666e44b261faf0cfb4a65d4d77ebb0d7
bhe2010 <bhevball@gmail.com>
2022-07-30 11:17:54 -0400

    post houston laptop website update

---
+++ project/portfolio/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Images
+
+# Create your views here.
+
+def home(request):
+    context = {}
+    images = Images.objects.all()
+    context["images"] = images
+    return render(request, "index.html", context)
